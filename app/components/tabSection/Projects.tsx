dac79b3d5b9f227d803d9ee7f18d6d0fd37a8c6f
Maitreyee Singh <bimalkumar@Mac.attlocal.net>
2025-10-04 17:55:13 -0400

    fix projectList import and prep build

--- app/components/tabSection/Projects.tsx
+++ app/components/tabSection/Projects.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { ProjectList } from "@/app/utils/projectList";  // <-- fixed path
+import { projects } from "@/app/utils/projectList";  // <-- use 'projects' not 'ProjectList'
 import { ProjectType } from "@/app/types";
 import { Project } from "../Project";
 
 export const Projects = () => {
   return (
     <main className="flex flex-col gap-20">
-      {ProjectList.map((project: ProjectType) => (
+      {projects.map((project: ProjectType) => (
         <Project
           key={`project-item-${project.title}`}
           {...project}
